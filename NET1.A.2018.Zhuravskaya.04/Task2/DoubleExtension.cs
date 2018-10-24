8aa4f9038933cb72b71f77a1c4585248938685bb
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-10-24 16:54:21 +0300

    Added Task2.

---
+++ NET1.A.2018.Zhuravskaya.04/Task2/DoubleExtension.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using static System.String;
+
+namespace Task2
+{
+    /// <summary>
+    /// DoubleExtension class contains extension methods for double.
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Extension method converts a double to IEEE 754 format.
+        /// </summary>
+        /// <param name="number">
+        /// Source number.
+        /// </param>
+        /// <returns>
+        /// Source number in IEEE 754 format.
+        /// </returns>
+        public static string ConvertToIEEE754String(this double number)
+        {
+            var numberOfBytes = 64;
+            var doubleToLong = new DoubleToLong() { PlaceForDouble = number };
+            var numberToLong = doubleToLong.PlaceForLong;
+
+            var doubleToIEEE754Format = new char[numberOfBytes];
+
+            for (int i = numberOfBytes - 1; i >= 0; --i)
+            {
+                if ((numberToLong & 1) == 1)
+                {
+                    doubleToIEEE754Format[i] = '1';
+                }
+                else
+                {
+                    doubleToIEEE754Format[i] = '0';
+                }
+
+                numberToLong >>= 1;
+            }
+            return Join("", doubleToIEEE754Format);
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct DoubleToLong
+        {
+            [FieldOffset(0)]
+            public double PlaceForDouble;
+
+            [FieldOffset(0)]
+            public readonly long PlaceForLong;
+        }
+    }
+}
