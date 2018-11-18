700daf0fef0a896875a3b20e15c86fd01f5198d6
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-11-18 15:47:21 +0300

    BinarySearchTree collection. Tests in process.

---
+++ NET1.A.2018.Zhuravskaya.16/BinarySearchTree/BinaryTreeNode.cs
@@ -0,0 +1,66 @@
+﻿namespace BinarySearchTree
+{
+    /// <summary>
+    /// Class contains node of binary search tree.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of information stored in the node.
+    /// </typeparam>
+    internal class BinaryTreeNode<T>
+    {
+        /// <summary>
+        /// Create an instance of BinaryTreeNode where parent, left and right nodes are null.
+        /// </summary>
+        /// <param name="info">
+        /// Node information.
+        /// </param>
+        public BinaryTreeNode(T info)
+        {
+            Info = info;
+            Parent = null;
+            Left = null;
+            Right = null;
+        }
+
+        /// <summary>
+        /// Create an instance of BinaryTreeNode where left and right nodes are null.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="parent">
+        /// Parent node.
+        /// </param>
+        public BinaryTreeNode(T info, BinaryTreeNode<T> parent)
+        {
+            Info = info;
+            Parent = parent;
+            Left = null;
+            Right = null;
+        }
+
+        /// <summary>
+        /// Parent node.
+        /// </summary>
+        public BinaryTreeNode<T> Parent { get; set; }
+
+        /// <summary>
+        /// Left node.
+        /// </summary>
+        public BinaryTreeNode<T> Left { get; set; }
+
+        /// <summary>
+        /// Right node.
+        /// </summary>
+        public BinaryTreeNode<T> Right { get; set; }
+
+        /// <summary>
+        /// Node information.
+        /// </summary>
+        public T Info { get; set; }
+
+        public override string ToString()
+        {
+            return "node: " + Info + ", left: " + (Left != null ? Left.Info.ToString() : "null") + ", right: " +
+                   (Right != null ? Right.Info.ToString() : "null");
+        }
+    }
+}
