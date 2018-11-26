48a0b319cd89669f67167f9733755ff8e51b60b6
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-11-26 22:09:07 +0300

    Added comments.

--- NET1.A.2018.Zhuravskaya.21/No6/SequenceGenerator.cs
+++ NET1.A.2018.Zhuravskaya.21/No6/SequenceGenerator.cs
@@ -3,8 +3,38 @@ using System.Collections.Generic;
 
 namespace No6
 {
+    /// <summary>
+    /// Sequence generator.
+    /// </summary>
     public static class SequenceGenerator
     {
+        /// <summary>
+        /// Generate the sequence.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Sequence element type.
+        /// </typeparam>
+        /// <param name="count">
+        /// Number of elements in the sequence.
+        /// </param>
+        /// <param name="firstElement">
+        /// first element of the sequence.
+        /// </param>
+        /// <param name="secondElement">
+        /// second element of the sequence.
+        /// </param>
+        /// <param name="nextElement">
+        /// formula for calculating the next element of the sequence.
+        /// </param>
+        /// <returns>
+        /// IEnumerable'1 T type sequence.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// count must not be less than 0.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// nextElement must not be null.
+        /// </exception>
         public static IEnumerable<T> Generate<T>(int count, T firstElement, T secondElement, Func<T, T, T> nextElement)
         {
             if (count < 0)
